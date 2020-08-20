812dbb2ca7085dcc6ebc220674dffcedb3c87918
Anshul Verma <52775504+Ansh-cloud@users.noreply.github.com>
2020-08-20 12:55:46 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -51,12 +51,6 @@ resource "aws_security_group" "db_sg" {
     security_groups = ["${aws_security_group.web_sg.id}"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "DBserver-sg"
